311025aed138a0681e82fc6dc617b626802b3a37
Aaron Hoffman <hoffman.aaron.d@gmail.com>
2013-03-16 11:38:06 -0500

    Create Test Project

---
+++ Source/StoneFinch.ReactiveExtensions.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reactive;
+using Microsoft.Reactive.Testing;
+
+namespace StoneFinch.ReactiveExtensions.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // using Microsoft.Reactive.Testing;
+            var testScheduler = new TestScheduler();
+
+
+        }
+    }
+}
